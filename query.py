81df5eb549579061c59c7ead2a6d482c263aad4f
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-07-25 04:49:25 +0330

    Create query.py

---
+++ query.py
@@ -0,0 +1,28 @@
+import requests
+
+def query_ip_api(input_file, output_file):
+    with open(input_file, 'r') as infile:
+        ip_addresses = infile.readlines()
+
+    results = []
+    for ip_address in ip_addresses:
+        ip_address = ip_address.strip()
+        try:
+            response = requests.get(f'http://ip-api.com/line/{ip_address}')
+            lines = response.text.split('\n')
+            if lines[0] == 'success':
+                country_code = lines[2]
+                results.append(f"{ip_address} - {country_code}")
+            else:
+                print(f"Query failed for IP: {ip_address}")
+        except requests.RequestException as e:
+            print(f"Request error for IP: {ip_address}: {e}")
+
+    with open(output_file, 'w') as outfile:
+        for result in results:
+            outfile.write(result + '\n')
+
+if __name__ == "__main__":
+    input_file = 'files/iplist.txt'
+    output_file = 'files/countries.txt'
+    query_ip_api(input_file, output_file)
